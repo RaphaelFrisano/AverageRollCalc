f8e5da8698391eff429ccc30e02ffd269eab6899
Raphael Frisano <54019563+RaphaelFrisano@users.noreply.github.com>
2022-08-05 11:46:49 +0200

    Release V1.1 Part 2

--- AverageRollCalc/Program.cs
+++ AverageRollCalc/Program.cs
@@ -24,31 +24,28 @@ namespace AverageRollCalc
             if (!File.Exists(folderPath + "\\charList.txt")){File.Create(folderPath + "\\charList.txt").Dispose();}
 
             // Check if a new version is available
-            string currentVersion = "1.0";
-            string pathToVersionFile = "https://github.com/RaphaelFrisano/AverageRollCalc/tree/main/AverageRollCalc/bin/Debug/currentVersion.txt";
+            string currentVersion = "1.1";
+            string pathToVersionFile = "https://raw.githubusercontent.com/RaphaelFrisano/AverageRollCalc/main/AverageRollCalc/bin/Release/currentVersion.txt";
             bool versionDif = versionDifferent(currentVersion, pathToVersionFile);
 
-            if (versionDif)
+            if(!versionDif)
             {
                 // Update Programm
                 if (MessageBox.Show("You are currently running version " + currentVersion + "\na new Version is available to download. Take you there?", "AverageRollCalc Updater", MessageBoxButtons.YesNo, MessageBoxIcon.Asterisk) == DialogResult.Yes)
                 {
-                    System.Diagnostics.Process.Start("https://github.com/RaphaelFrisano/AverageRollCalc/tree/main/AverageRollCalc/bin/release/AverageRollCalc.exe");
+                    System.Diagnostics.Process.Start("https://github.com/RaphaelFrisano/AverageRollCalc/tree/main/AverageRollCalc/bin/Release");
                 }
             }
-            else
-            {
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new Form1());
-            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
             
         }
 
         public static bool versionDifferent(string currentVersion, string pathToVersionFile)
         {
-            WebClient myBotNewVersionClient = new WebClient();
-            Stream stream = myBotNewVersionClient.OpenRead(pathToVersionFile);
+            WebClient NewVersionClient = new WebClient();
+            Stream stream = NewVersionClient.OpenRead(pathToVersionFile);
             StreamReader reader = new StreamReader(stream);
             string onlineVersion = reader.ReadToEnd();
 
